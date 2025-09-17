a481ef5018fd5cd4cd06bbb70317bc136862b69e
Lakshan Fernando <162671765+lakshanFernando2003@users.noreply.github.com>
2025-09-17 18:23:39 +0530

    Add failing tests BornToFail and DieToFail

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,6 +35,23 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  // tests added for fail ------------------------------------------------
+
+  @Test
+  public void BornToFail() {
+    assertTrue(false);
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+  
+
+  @Test
+  public void DieToFail() {
+    assertTrue(false);
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+// ---------------------------------------------------------------------
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
